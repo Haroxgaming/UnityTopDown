35c0fa821ba63bbe6522a538ebb7e6bb4dd89a84
Haroxgaming <mathis.denamur.pro@gmail.com>
2025-01-06 15:26:42 +0100

    push test
    push test

---
+++ Assets/Dev/Mathis/CamController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public Transform player;
+	public float smoothSpeed;
+
+	private Vector3 targetPos, newPos;
+
+	public Vector3 minPos, maxPos;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+    }
+}
