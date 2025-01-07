c1821d22139436316f7aae0effe3ed73956dfe4f
Haroxgaming <mathis.denamur.pro@gmail.com>
2025-01-07 09:41:31 +0100

    Fix cambug
    Fix cambug

--- Assets/Dev/Mathis/CamController.cs
+++ Assets/Dev/Mathis/CamController.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CamController : MonoBehaviour
 {
 	public Transform player;
 	public float smoothSpeed;
