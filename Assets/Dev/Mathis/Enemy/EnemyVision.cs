4567b12cac65b4c9df76821f93f0d813e11f789d
Haroxgaming <mathis.denamur.pro@gmail.com>
2025-01-07 14:51:37 +0100

    Enemy line of sight
    Enemy line of sight

--- Assets/Dev/Mathis/Enemy/EnemyVision.cs
+++ Assets/Dev/Mathis/Enemy/EnemyVision.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyVision : MonoBehaviour
 {
     public float DetectRange = 10;
+    public float DetectAngle = 45;
     private bool isInAngle, isInRange, isNotHidden;
 
     public GameObject Player;
-    TMP_Text RangeText
+
+    public TMP_Text RangeText, HiddenText, AngleText, DetectedText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,13 +19,14 @@ public class EnemyVision : MonoBehaviour
     }
 
     // Update is called once per frame
+    
     void Update()
     {
         isInAngle = false;
         isInRange = false;
         isNotHidden = false;
 
-        if (Vector3.distance(transform.position, Player.transform.position) < DetectRange)
+        if (Vector3.Distance(transform.position, Player.transform.position) < DetectRange)
         {
             isInRange = true;
             RangeText.text = "In Range";
@@ -32,6 +36,46 @@ public class EnemyVision : MonoBehaviour
         {
             RangeText.text = "Not In Range";
             RangeText.color = Color.green;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, (Player.transform.position - transform.position), out hit,
+                Mathf.Infinity))
+        {
+            isNotHidden = true;
+            HiddenText.text = "Not Hidden";
+            HiddenText.color = Color.red;
+        }
+        else
+        {
+            HiddenText.text = "Hidden";
+            HiddenText.color = Color.green;
+        }
+
+        Vector3 side1 = Player.transform.position - transform.position;
+        Vector3 side2 = transform.forward;
+        float angle = Vector3.SignedAngle(side1, side2, Vector3.up);
+        if (angle < DetectAngle && angle > -1 * DetectAngle)
+        {
+            isInAngle = true;
+            AngleText.text = "In Vision Angle";
+            AngleText.color = Color.red;
+        }
+        else
+        {
+            AngleText.text = "Not In Vision Angle";
+            AngleText.color = Color.green;
+        }
+
+        if (isInAngle && isInRange && isNotHidden)
+        {
+            DetectedText.text = "Player Detected";
+            DetectedText.color = Color.red;
+        }
+        else
+        {
+            DetectedText.text = "Player Not Detected";
+            DetectedText.color = Color.green;
         }
     }
 }
