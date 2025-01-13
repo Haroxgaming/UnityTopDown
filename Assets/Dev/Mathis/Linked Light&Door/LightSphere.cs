604eda039acf5d71c0d2aac07af386f263824ba0
Haroxgaming <mathis.denamur.pro@gmail.com>
2025-01-13 09:51:32 +0100

    Envoie a evan
    Envoie a evan

--- Assets/Dev/Mathis/Linked Light&Door/LightSphere.cs
+++ Assets/Dev/Mathis/Linked Light&Door/LightSphere.cs
@@ -2,31 +2,10 @@ using UnityEngine;
 
 public class LightSphere : MonoBehaviour
 {
-    public GameObject lightOn, lightOff, switchOn, switchOff;
-    public bool toggle;
-    public AudioSource switchSound;
-
-    void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("MainCamera"))
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                if (toggle == true)
-                {
-                    lightOn.SetActive(true);
-                    lightOff.SetActive(false);
-                    switchOn.SetActive(true);
-                    switchOff.SetActive(false);
-                }
-                else
-                {
-                    lightOn.SetActive(false);
-                    lightOff.SetActive(true);
-                    switchOn.SetActive(false);
-                    switchOff.SetActive(true);
-                }
-            }
-        }
-    }
+    public GameObject colorChange;
+    
+    public Material mat
+    
+    colorChange.
+    
 }
